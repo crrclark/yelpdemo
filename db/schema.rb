8d04a578483ea84b57fc17ba97780cb934f3c6fa
Craig Clark <craigrobertclark@gmail.com>
2018-03-15 18:28:32 +0000

    add fileds to usres

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308001244) do
+ActiveRecord::Schema.define(version: 20180315180808) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20180308001244) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
